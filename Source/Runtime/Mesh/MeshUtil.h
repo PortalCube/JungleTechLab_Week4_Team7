4079719ce50d38f9881a43face64f253d84a0c94
Kim Gyeongjae <skyneon77@gmail.com>
2026-09-18 21:22:40 +0900

    chore: 추가 정리

--- Source/Runtime/Mesh/MeshUtil.h
+++ Source/Runtime/Mesh/MeshUtil.h
@@ -6,18 +6,29 @@ class FRenderResourceLibrary;
 namespace MeshUtil
 {
 bool CreateCubeMesh(FRenderer &Renderer, FRenderResourceLibrary &Library);
-bool CreateCylinderMesh(FRenderer &Renderer, FRenderResourceLibrary &Library,
-                        float Height, unsigned int SliceCount, float TopRadius,
-                        float BottomRadius);
+bool CreateCylinderMesh(
+    FRenderer &Renderer,
+    FRenderResourceLibrary &Library,
+    float Height,
+    unsigned int SliceCount,
+    float TopRadius,
+    float BottomRadius
+);
 bool CreateConeMesh(FRenderer &Renderer, FRenderResourceLibrary &Library);
-bool CreateSpotlightConeMesh(FRenderer &Renderer,
-                             FRenderResourceLibrary &Library);
+bool CreateSpotlightConeMesh(
+    FRenderer &Renderer,
+    FRenderResourceLibrary &Library
+);
 bool CreateArrowMesh(FRenderer &Renderer, FRenderResourceLibrary &Library);
 bool CreateCircleMesh(FRenderer &Renderer, FRenderResourceLibrary &Library);
-bool CreateRotationGizmoMesh(FRenderer &Renderer,
-                             FRenderResourceLibrary &Library);
-bool CreateSquareArrowMesh(FRenderer &Renderer,
-                           FRenderResourceLibrary &Library);
+bool CreateRotationGizmoMesh(
+    FRenderer &Renderer,
+    FRenderResourceLibrary &Library
+);
+bool CreateSquareArrowMesh(
+    FRenderer &Renderer,
+    FRenderResourceLibrary &Library
+);
 bool CreateGridMesh(FRenderer &Renderer, FRenderResourceLibrary &Library);
 bool CreateSphereMesh(FRenderer &Renderer, FRenderResourceLibrary &Library);
 bool CreateLineMesh(FRenderer &Renderer, FRenderResourceLibrary &Library);
