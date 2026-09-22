ca3aa57e1bf3e6250181915ed6d057a6e06a6f6a
SeongminHwang <jameshwang16@gmail.com>
2026-09-22 22:41:29 +0900

    Add oiia cat

--- Source/Editor/Core/EditorConstant.h
+++ Source/Editor/Core/EditorConstant.h
@@ -10,6 +10,7 @@
 #include "Runtime/Actors/AAnimatedBillboardActor.h"
 #include "Runtime/Actors/ASpotlightActor.h"
 #include "Runtime/Actors/ATextRenderActor.h"
+#include "Runtime/Actors/ACatActor.h"
 
 namespace EditorConstant
 {
@@ -26,6 +27,7 @@ namespace EditorConstant
 	   AAnimatedBillboardActor::StaticClass(),
 	   ASpotlightActor::StaticClass(),
 	   ATextRenderActor::StaticClass(),
+	   ACatActor::StaticClass(),
 	};
 
 }
