bd1689ba638c6ba391b3ebd964bdce5ce1d0de6d
SeongminHwang <jameshwang16@gmail.com>
2026-09-22 20:50:58 +0900

    Remove duplicate vertices

--- Source/Runtime/Parser/FObjParser.h
+++ Source/Runtime/Parser/FObjParser.h
@@ -3,6 +3,7 @@
 #include "Source/Runtime/Math/FVector2.h"
 #include "Source/Runtime/Math/FVector4.h"
 #include "Source/Runtime/Core/TArray.h"
+#include "Source/Runtime/Core/TMap.h"
 #include "Source/Runtime/Core/FString.h"
 #include "Source/Runtime/Core/IntTypes.h"
 #include "Source/Runtime/Rendering/Vertices.h"
@@ -16,6 +17,11 @@ struct FObjIndex
 	int v = 0;
 	int vt = 0;
 	int vn = 0;
+
+	bool operator==(const FObjIndex& Rhs) const
+	{
+		return v == Rhs.v && vt == Rhs.vt && vn == Rhs.vn;
+	}
 };
 
 struct FRawObjData
